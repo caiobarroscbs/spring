f9a41d4dc6252c91995e99ee2275bc2b4f7b6f0b
caiobarroscbs <caiobarros.contato@gmail.com>
2022-11-28 10:06:00 -0300

    service

---
+++ empresa/src/main/java/org/soulcodeacademy/empresa/services/TesteService.java
@@ -0,0 +1,30 @@
+package org.soulcodeacademy.empresa.services;
+
+import org.soulcodeacademy.empresa.repositories.DependenteRepository;
+import org.soulcodeacademy.empresa.repositories.EmpregadoRepository;
+import org.soulcodeacademy.empresa.repositories.EnderecoRepository;
+import org.soulcodeacademy.empresa.repositories.ProjetoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class TesteService {
+    @Autowired
+    private EmpregadoRepository empregadoRepository;
+
+    @Autowired
+    private DependenteRepository dependenteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ProjetoRepository projetoRepository;
+
+    @PostConstruct
+    public void testarEntidades() {
+        System.out.println("Olá!");
+    }
+}
